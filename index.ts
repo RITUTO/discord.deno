0b11eb9773408f7bcddaeba18ecde52704682f18
RITUTO <144442942+RITUTO@users.noreply.github.com>
2024-09-29 20:32:05 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,4 +1,11 @@
 import {Client} from "./src/client/client.ts"
 const client = new Client()
-
+client.on("ready", () =>{
+    console.log("botを起動しました")
+})
+client.on("messageCreate",(message) =>{
+   if (message.content == "test"){
+    message.reply("Hello World!")
+   }
+})
 client.login("token")
