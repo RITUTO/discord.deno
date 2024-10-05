245f2636b2a9a290557e5ad5897ff9c7ec3b9ad0
RITUTO <144442942+RITUTO@users.noreply.github.com>
2024-10-05 15:38:53 +0900

    Add files via upload

--- types/events.d.ts
+++ types/events.d.ts
@@ -1,7 +1,9 @@
-import {message} from "../src/client/classes/messageclass.ts"
+import {message} from "../src/classes/messageclass.ts"
 export interface Events {
     [ready]:  void;
     [messageCreate]:message
+    [Debug]:string
   }
 export const ready: "ready";
 export const messageCreate: "messageCreate";
+export const Debug: "Debug";
