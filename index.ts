91e4d2667503be90530d994fce66268156b81cc7
RITUTO <144442942+RITUTO@users.noreply.github.com>
2024-10-05 14:36:43 +0900

    Update index.ts

--- index.ts
+++ index.ts
@@ -9,3 +9,25 @@ client.on("messageCreate",(message) =>{
    }
 })
 client.login("token")
+import {Client,EmbedBuilder} from "./src/index.ts"
+const client = new Client({Intents:["GUILDS","MESSAGE_CONTENT"]})
+client.on("ready", () =>{
+    console.log("botを起動しました")
+})
+client.on("messageCreate",(message) =>{
+   if (message.content == "test"){
+    message.reply("Hello World!")
+   }
+   if (message.content == "embed"){
+    const embed = new EmbedBuilder()
+    .setTitle("embed")
+    .setDescription("despcription")
+    .addField("fild1","fild",true)
+    
+    message.reply({embeds:[embed]})
+   }
+})
+client.on("Debug",(string) =>{
+    console.log(string)
+})
+client.login("token")
