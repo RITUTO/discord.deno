4074a98709bf6e7548fa64687668fb7a2fddfc38
RITUTO <144442942+RITUTO@users.noreply.github.com>
2024-11-02 07:51:57 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-import { Client } from "./client/client.ts";
-import { EmbedBuilder } from "./builder/embedbuilder.ts";
-import { ButtonBuilder } from "./builder/buttonbuilder.ts"
-import { ActionRowBuilder } from "./builder/ActionRowBuilder.ts";
-export {Client,EmbedBuilder,ButtonBuilder,ActionRowBuilder} 
+//
