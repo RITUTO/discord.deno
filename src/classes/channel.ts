c60574358ba5f14d457d698f66f039986d91e4d3
RITUTO <144442942+RITUTO@users.noreply.github.com>
2024-11-02 07:42:04 +0900

    Update channel.ts

--- src/classes/channel.ts
+++ src/classes/channel.ts
@@ -12,7 +12,8 @@ export class Channel {
     message_count!:number|undefined
     permissions!:string|undefined
     users!:string|undefined
-    constructor(channelstructure:any){
+    constructor(channelstructure:any,clientclass:any){
+        this.clientclass = clientclass
         this.id = channelstructure.id
         this.type = channelstructure.type
         this.guild_id = channelstructure.guild_id
@@ -26,6 +27,48 @@ export class Channel {
         // deno-lint-ignore ban-ts-comment
         //@ts-ignore
         this.users = channelstructure.recipients?.map(m => new user(m))
+    }
+     async send(args: string | { content?: string; embeds?: EmbedBuilder[] | object[]; components?: ActionRowBuilder | ActionRowBuilder[] | any[] }) {
+      const message: any = {};
+    
+      if (typeof args === 'string') {
+        message.content = args;
+        message.tts = false;
+      } else {
+        message.content = args.content;
+        message.tts = false;
+
+    
+
+        if (args.embeds) {
+          if (args.embeds instanceof EmbedBuilder) {
+              message.embeds = [args.embeds.build()];
+            } else {
+                message.embeds = args.embeds; 
+        }
+            }
+        if (args.components) {
+          if (Array.isArray(args.components)) {
+            message.components = args.components.map(component => {
+              if (component instanceof ActionRowBuilder) {
+                return component.build(); 
+              } else if (typeof component === 'object') {
+                return component;
+              }
+              return component;
+            });
+          } else if (args.components instanceof ActionRowBuilder) {
+            message.components = [args.components.build()]; 
+          } else if (typeof args.components === 'object') {
+            message.components = [args.components];
+          }
+        }
+
+      }
+      await this.#clientclass.request(`channels/${this.channel_id}/messages`, {
+        method: "POST",
+        body: JSON.stringify(message),
+      });
     }
     
 }
